11fe83b2cea2adb20495278eafb5254d708c6f56
brad <b.dossantospatatas@it-students.fr>
2021-06-08 11:37:05 +0200

    :sparkles: Display a Form connected to the BackEnd

---
+++ src/views/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Form/>
+  </div>
+</template>
+
+<script>
+import Form from '../components/Form';
+export default {
+  name: "Info",
+  components: {
+    Form
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
