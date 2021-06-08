b5d501c9044f1f6a230c74c6458d8ad2be7fb6e4
Alix <a.boufflers@it-students.fr>
2021-06-08 09:18:57 +0200

    :sparkles: add clic event buying logic, and all resources

--- src/components/ClicContainer.vue
+++ src/components/ClicContainer.vue
@@ -1,29 +1,164 @@
 <template lang="">
     <div>
-        <h1>vous avez cliquez : {{nbrClic}} fois</h1>
-        <button @click="clic">cliquez ici</button>
+        <div @click="clic" class="clicScreen">
+            <h1>Budget : {{budget}} Radis</h1>
+            <h4>radis par clic : {{radisPerClic * multiplicator}}</h4>
+            <h4>Tous les investissements :</h4>
+            <p>{{userResources[0]}}</p>
+
+            <div v-for="resource in allResources" :key="resource.name" @click="buyResource(resource.cost), addToMultiplicator(resource.multiplicator)">
+                <Resource v-if="displayResource(resource.cost)" :cost="resource.cost" :name="resource.name" :addToClic="resource.addToClick"/>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
+import Resource from '../components/Resource'
 export default {
     name:"ClicContainer",
-
     data() {
         return {
-            nbrClic : 0
+            budget : 0,
+            multiplicator: 1,
+            radisPerClic: 1,
+            userResources : {
+                player : 0,
+            },
+            allResources : [
+                {
+                    name : "Joueuse",
+                    cost : "10",
+                    addToClick: "1",
+                    multiplicator : 1
+                },
+                {
+                    name : "Equipe amatrice",
+                    cost : "50",
+                    addToClick: "5",
+                    multiplicator : 5
+                },
+                {
+                    name : "Entraineuse",
+                    cost : "100",
+                    addToClick: "10",
+                    multiplicator : 10
+                },
+                {
+                    name : "Terrain de foot",
+                    cost : "150",
+                    addToClick: "15",
+                    multiplicator : 15
+                },
+                {
+                    name : "Equipe de L3",
+                    cost : "200",
+                    addToClick: "20",
+                    multiplicator : 20
+                },
+                {
+                    name : "Manageuse",
+                    cost : "250",
+                    addToClick: "25",
+                    multiplicator : 25
+                },
+                {
+                    name : "Tribune",
+                    cost : "300",
+                    addToClick: "30",
+                    multiplicator : 30
+                },
+                {
+                    name : "Equipe de L2",
+                    cost : "350",
+                    addToClick: "35",
+                    multiplicator : 35
+                },
+                {
+                    name : "Sponsors",
+                    cost : "400",
+                    addToClick: "40",
+                    multiplicator : 40
+                },
+                {
+                    name : "Joueuse vedette",
+                    cost : "450",
+                    addToClick: "45",
+                    multiplicator : 45
+                },
+                {
+                    name : "Stade",
+                    cost : "500",
+                    addToClick: "50",
+                    multiplicator : 50
+                },
+                {
+                    name : "Equipe de L1",
+                    cost : "550",
+                    addToClick: "55",
+                    multiplicator : 55
+                },
+                {
+                    name : "Equipe nationale",
+                    cost : "600",
+                    addToClick: "60",
+                    multiplicator : 60
+                },
+                {
+                    name : "Euro 2020",
+                    cost : "1000",
+                    addToClick: "100",
+                    multiplicator : 100
+                },
+            ]
         }
     },
 
     methods: {
         clic(){
-            this.nbrClic ++
+            this.budget += ((parseInt(this.radisPerClic) * parseInt(this.multiplicator)))
+        },
+
+        buyResource(cost){
+            this.budget -= (parseInt(cost) + (parseInt(this.radisPerClic) * parseInt(this.multiplicator)))
+        },
+
+        displayResource(cost){
+            if(this.budget >= parseInt(cost)){
+                return true
+            }else{
+                return false
+            }
+        },
+
+        addToMultiplicator(nbr){
+            this.multiplicator += nbr
         }
     },
 
+    components: {
+        Resource
+    }
 }
+
 </script>
 
-<style lang="">
-    
+<style >
+    .clicScreen{
+        background: rgb(177, 177, 177);
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+    .resources{
+        background: rgb(255, 217, 0);
+        padding: 10px;
+    }
+    .resourcesContent{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+    }
 </style>
